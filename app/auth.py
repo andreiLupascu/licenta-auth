1ccc4c9950c9374d82c855b12eb2016fb9037a4f
Andrei Lupascu <andreilupascu97@yahoo.com>
2019-10-26 19:05:13 +0300

    completed authorization functionality

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import os
 import base64
 from flask_jwt_extended import (
     JWTManager,
@@ -9,16 +8,11 @@ from passlib.hash import bcrypt
 import pymysql
 
 app = Flask(__name__)
+app.config.from_envvar('FLASK_CONFIG_FILE')
 
-app.config['JWT_SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
-app.config['DB-HOST'] = os.getenv('APP_DB_HOST')
-app.config['DB-PORT'] = os.getenv('APP_DB_PORT')
-app.config['DB-USER'] = os.getenv('APP_DB_USER')
-app.config['DB-NAME'] = os.getenv('APP_DB_NAME')
-app.config['DB-PASS'] = os.getenv('APP_DB_PASS')
 jwt = JWTManager(app)
 
-
+#TODO maybe change JWT expiration payload
 @app.route('/api/auth/login', methods=['POST'])
 def login():
     credentials = request.json.get('credentials')
@@ -35,12 +29,12 @@ def login():
 
 
 def verify_credentials(username, password):
-    port = int(app.config['DB-PORT'])
-    conn = pymysql.connect(host=app.config['DB-HOST'],
+    port = int(app.config['DB_PORT'])
+    conn = pymysql.connect(host=app.config['DB_HOST'],
                            port=port,
-                           user=app.config['DB-USER'],
-                           passwd=app.config['DB-PASS'],
-                           db=app.config['DB-NAME'])
+                           user=app.config['DB_USER'],
+                           passwd=app.config['DB_PASS'],
+                           db=app.config['DB_NAME'])
     with conn.cursor(pymysql.cursors.DictCursor) as cur:
         cur.execute('SELECT * FROM Users WHERE username = %s;', (username,))
         db_pass = cur.fetchone()['Password']
